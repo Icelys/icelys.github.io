75843ea902d29a552e9a50c90b503773f4215a05
Icely <icely.scratch@gmail.com>
2016-01-12 18:04:00 -0500

    Create embedded.php
    Just a test...

---
+++ embedded.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Embedded Project</title>
+</head>
+<body>
+<php
+$id=$_GET["id"];
+echo '<iframe allowtransparency="true" width="485" height="402" src="//scratch.mit.edu/projects/embed/$id/?autostart=false" frameborder="0" allowfullscreen></iframe>'
+?>
+</body>
+</html>
